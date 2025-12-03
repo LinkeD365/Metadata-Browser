f3d65e2005eae10ed3d9dd93ddef07c0d405cdbd
LinkeD365 <carl.cookson@gmail.com>
2025-12-03 17:58:15 +0000

    feat: Implement Solutions component and integrate solutions metadata handling in TableDetail

--- src/model/solution.tsx
+++ src/model/solution.tsx
@@ -4,11 +4,18 @@ export class Solution {
   solutionId: string;
   solutionName: string;
   uniqueName: string;
+  description?: string;
+  version?: string;
+  isManaged?: boolean;
+  subcomponents?:boolean;
   
+  attributes: { attributeName: string; attributeValue: string }[];
+
   constructor() {
     this.solutionId = "";
     this.solutionName = "";
     this.uniqueName = "";
+    this.attributes = [];
     makeAutoObservable(this);
   }
 }
